c6bb0a84d21d30b119b6ddf7e60748de6d7afdd8
Peter Kim <kroits09@gmail.com>
2018-06-27 13:03:07 +0900

    Add creatPost fn to actions

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
 export const FETCH_POSTS = 'fetch_posts';
+export const CREATE_POST = 'create_post';
 
 const ROOT_URL = 'http://reduxblog.herokuapp.com/api';
 const API_KEY = '?key=peterwns1231';
@@ -12,4 +13,14 @@ export const fetchPosts = () => {
     type: FETCH_POSTS,
     payload: request
   };
+}
+
+export const createPost = (values, callback) => {
+  const request = axios.post(`${ROOT_URL}/posts${API_KEY}`, values)
+    .then(() => callback());
+
+  return {
+    type: CREATE_POST,
+    payload: request
+  }
 }
