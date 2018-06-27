d2d9aec4cf1102f5c304ebf70fad7a3d26774bb9
Peter Kim <kroits09@gmail.com>
2018-06-27 13:02:28 +0900

    Add redirection after posting

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -1,23 +1,34 @@
 import React, { Component } from 'react';
 import { Field, reduxForm } from 'redux-form';
+import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { createPost } from '../actions';
 
 class PostsNew  extends Component {
   renderField(field) {
+    const { meta: { touched, error } } = field;
+    const className = `form-group ${touched && error ? 'has-danger' : ''}`
+
     return (
-      <div className="form-group">
+      <div className={className}>
         <label>{field.label}</label>
         <input 
           className="form-control"
           type="text"
           {...field.input}
         />
-        {field.meta.error}
+        <div className="text-help">
+          {touched && error}
+        </div>
+       
       </div>
     );
   }
 
   onSubmit(values) {
-    console.log(values);
+    this.props.createPost(values, () => {
+      this.props.history.push('/');
+    }); 
   }
 
   render() {
@@ -42,6 +53,7 @@ class PostsNew  extends Component {
             component={this.renderField}
           />
           <button type="submit" className="btn btn-primary">Submit</button>
+          <Link to="/" className="btn btn-danger">Cancel</Link>
         </form>
       </div>
     );
@@ -54,8 +66,8 @@ const validate = (values) => {
   if (!values.title || values.title.length < 3) {
     errors.title = "Enter a title";
   }
-  if (!values.categoires) {
-    errors.categoires = "Enter some categories";
+  if (!values.categories) {
+    errors.categories = "Enter some categories";
   }
   if (!values.content) {
     errors.content = 'Enter some content please';
@@ -66,4 +78,6 @@ const validate = (values) => {
 export default reduxForm({
   validate,
   form: 'PostsNewForm'
-})(PostsNew);
+})(
+  connect(null, {createPost})(PostsNew)
+);
